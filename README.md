4850c5f73dbb25e771e8e32020281917d313f3c7
Tim K <timprogrammer@rambler.ru>
2019-04-26 18:40:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# TXLin
+TXLin is a TXLib-compatible library that provides a simple way to draw graphics in C++ programs. Unlike TXLib, which uses GDI and, thus, works only on Windows, TXLin uses SDL2 and, as such, can work on Linux and Mac OS X.
+
+## Building
+Just like TXLib, TXLin does not require to be built as it is a one-header library. However, this library depends on SDL2, which needs to be installed manually.
+
+On Ubuntu Linux, you should be able to install it like this:
+```
+sudo apt install libsdl2-dev
+```
+On Mageia/OpenMandriva Linux, this command will install SDL2 development files and headers:
+```
+urpmi 'pkgconfig(sdl2)'
+```
+
+Then, when linking your applications, you should add `sdl2-config --cflags --libs` to your compiler flags. For example:
+```
+clang++ my_txlin_program.cpp -o my_txlin_program -I. -std=c++0x `sdl2-config --cflags --libs`
+``` 
+
+## Porting TXLib apps to TXLin
+It's easy. Just replace ``#include "TXLib.h"`` with ``#include "TXLin.h"`` and you're good to go.
---
+++ TXLin.h
@@ -0,0 +1,377 @@
+#include <SDL.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <cctype>
+#include <cmath>
+#include <cstdint>
+
+#define HWND Uint32
+#define HDC SDL_Renderer*
+#define HPEN SDL_Renderer*
+#define RGBQUAD Uint32
+
+#define TXLIN_VERSION "TXLin [Ver: 1.74a, Rev: 106, Date: 2014-04-26 00:00:00]"
+#define TXLIN_VERSIONNUM 0x174a0
+
+// compatibility defines
+#define _TX_VERSION TXLIN_VERSION
+#define _TX_VER TXLIN_VERSIONNUM
+
+// debug define
+#ifdef TXLIN_DEBUG
+#define DBGOUT std::cerr << "[TXLin/DEBUG/" << __LINE__ << "] "
+#else
+#define DBGOUT std::stringstream()
+#endif
+#define DBGINT(var) DBGOUT << #var << " = " << var << std::endl
+
+struct POINT {
+    double x;
+    double y;   
+};
+
+struct COLORREF {
+    int r;
+    int g;
+    int b;
+};
+
+static bool txLinUnportableHasInitializedTXLinInThisContext = false;
+static HWND txLinUnportableRecentlyCreatedWindow = -1;
+
+inline HWND txWindow() {
+    DBGINT(txLinUnportableRecentlyCreatedWindow);
+    return txLinUnportableRecentlyCreatedWindow;
+}
+
+inline SDL_Point* txLinUnportablePointCapsToSDL(const POINT* points, int pointsCount) {
+    SDL_Point* pointsFinal = (SDL_Point*)(calloc(pointsCount, sizeof(SDL_Point)));
+    for (int i = 0; i < pointsCount; i++) {
+        pointsFinal[i].x = (int)(points[i].x);
+        pointsFinal[i].y = (int)(points[i].y);
+    }
+    return pointsFinal;
+}
+
+inline bool txLinUnportableInitSDL() {
+    return (SDL_Init(SDL_INIT_VIDEO) == 0);
+}
+
+inline int txLinUnportableModule(int number) {
+    if (number < 0)
+        return ((number) * (-1));
+    return number;
+}
+
+inline void txRedrawWindow() {
+    SDL_UpdateWindowSurface(SDL_GetWindowFromID(txWindow()));
+}
+
+inline HWND txCreateWindow(double sizeX, double sizeY, bool centered = true) {
+    if (txLinUnportableHasInitializedTXLinInThisContext && txLinUnportableInitSDL() == false)
+        return -1;
+    int x = SDL_WINDOWPOS_CENTERED;
+    int y = SDL_WINDOWPOS_CENTERED;
+    if (centered == false) {
+        x = 2;
+        y = 2;
+    }
+    SDL_Window* window = SDL_CreateWindow("TXLin", x, y, (int)(sizeX), (int)(sizeY), SDL_WINDOW_SHOWN);
+    txLinUnportableRecentlyCreatedWindow = SDL_GetWindowID(window);
+    return txLinUnportableRecentlyCreatedWindow;
+}
+
+inline HDC txDC();
+
+inline bool txSetDefaults(HDC dc = txDC()) {
+    DBGOUT << "called txSetDefaults" << std::endl;
+    SDL_Renderer* rendererContext = dc;
+    if (rendererContext == nullptr)
+        return false;
+    SDL_SetRenderDrawColor(rendererContext, 255, 255, 255, 0);
+    //SDL_RenderClear(rendererContext);
+    return true;
+}
+
+HDC txDC() {
+    DBGOUT << "called txDC()" << std::endl;
+    SDL_Window* window = SDL_GetWindowFromID(txWindow());
+    if (window == nullptr)
+        return nullptr;
+    SDL_Renderer* resultRenderer = SDL_GetRenderer(window);
+    if (resultRenderer == nullptr) {
+        DBGOUT << "regenerating renderer" << std::endl;
+        resultRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+        txSetDefaults(resultRenderer);
+        txRedrawWindow();
+        //txSetDefaults(resultRenderer);
+    }
+    return resultRenderer;
+}
+
+inline RGBQUAD* txVideoMemory(HDC dc = txDC()) {
+    //if (dc == nullptr)
+        return nullptr;
+    //return (RGBQUAD*)(dc->pixels);
+}
+
+
+inline bool txOK() {
+    return txLinUnportableHasInitializedTXLinInThisContext;
+}
+
+
+inline POINT txGetExtent (HDC dc = txDC()) {
+    POINT result;
+    result.x = 0.0;
+    result.y = 0.0;
+    int xSet = 0;
+    int ySet = 0;
+    if (SDL_GetRendererOutputSize(dc, &xSet, &ySet) != 0)
+        return result;
+    result.x = (double)(xSet);
+    result.y = (double)(ySet);
+    return result;
+}
+
+
+inline int txGetExtentX (HDC dc = txDC()) {
+    return (int)(txGetExtent(dc).x);
+}
+
+
+inline int txGetExtentY (HDC dc = txDC()) {
+     return (int)(txGetExtent(dc).y);
+}
+
+
+inline const char* txVersion() {
+    return TXLIN_VERSION;
+}
+
+inline unsigned txVersionNumber() {
+    return TXLIN_VERSIONNUM;
+}
+
+const char* txGetModuleFileName (bool fileNameOnly = true) {
+    // currently does not work
+    (void)(fileNameOnly);
+    return nullptr;
+}
+
+COLORREF RGB (int red, int green, int blue) {
+    COLORREF result = { red, green, blue };
+    return result;
+}
+
+HPEN txSetColor (COLORREF color, double thickness = 1, HDC dc = txDC()) {
+    (void)(thickness);
+    SDL_Renderer* rendererContext = dc;
+    if (rendererContext == nullptr)
+        return nullptr;
+    SDL_SetRenderDrawColor(rendererContext, color.r, color.g, color.b, 0);
+    return rendererContext;
+}
+
+COLORREF txColor (double red, double green, double blue) {
+    COLORREF color = { (int)(red), (int)(green), (int)(blue) };
+    txSetColor(color);
+    return color;
+}
+
+COLORREF txGetColor (HDC dc = txDC()) {
+    COLORREF result = { 255, 255, 255 };
+    if (dc == nullptr)
+        return result;
+    Uint8 rBit, gBit, bBit;
+    SDL_GetRenderDrawColor(dc, &rBit, &gBit, &bBit, nullptr);
+    result.r = (int)(rBit);
+    result.g = (int)(gBit);
+    result.b = (int)(bBit);
+    return result;
+}
+
+COLORREF txFillColor (double red, double green, double blue) {
+    return txColor(red, green, blue);
+} 
+
+COLORREF txGetFillColor (HDC dc = txDC()) {
+    return txGetColor(dc);
+}
+
+unsigned txExtractColor (COLORREF color, COLORREF component) {
+    // does not work yet
+    return 0;
+}
+
+COLORREF txRGB2HSL (COLORREF rgbColor) {
+    // does not work yet
+    return rgbColor;
+}
+
+COLORREF txHSL2RGB (COLORREF hslColor) {
+    // does not work yet
+    return hslColor;
+}
+
+
+bool txClear (HDC dc = txDC()) {
+    if (dc == nullptr)
+        return false;
+    SDL_RenderClear(dc);
+    return true;
+}
+
+
+inline bool txSetPixel (double x, double y, COLORREF color = txGetColor(), HDC dc = txDC()) {
+    if (dc == nullptr)
+        return false;
+    txSetColor(color);
+    SDL_RenderDrawPoint(dc, (int)(x), (int)(y));
+    return true;
+}
+
+inline bool txPixel (double x, double y, double red, double green, double blue, HDC dc = txDC()) {
+    return txSetPixel(x, y, RGB((int)(red), (int)(green), (int)(blue)), dc);
+}
+
+inline COLORREF txGetPixel (double x, double y, HDC dc = txDC()) {
+    /*
+    Uint32* arrayOfPixels = txVideoMemory(dc);
+    Uint32 pixel = arrayOfPixels[((int)(y) * txGetExtentX(dc)) + (int)(x)];
+    Uint8 rBit, gBit, bBit;
+    SDL_GetRGB(pixel, nullptr, &rBit, &gBit, &bBit);
+    COLORREF result = { (int)(rBit), (int)(gBit), (int)(bBit) };
+    */
+    COLORREF result = { 255, 255, 255 };
+    return result;
+}
+
+inline bool txLine (double x0, double y0, double x1, double y1, HDC dc = txDC()) {
+    if (dc == nullptr) {
+        DBGOUT << "dc is nullptr, return false" << std::endl;
+        return false;
+    }
+    DBGINT(x0);
+    DBGINT(y0);
+    DBGINT(x1);
+    DBGINT(y1);
+    DBGOUT << "SDL_RenderDrawLine" << std::endl;
+    SDL_RenderDrawLine(dc, (int)(x0), (int)(y0), (int)(x1), (int)(y1));
+    DBGOUT << "txRedrawWindow()" << std::endl;
+    txRedrawWindow(); 
+    DBGOUT << "return true;" << std::endl;
+    return true;
+}
+
+inline bool txRectangle (double x0, double y0, double x1, double y1, HDC dc = txDC()) {
+    if (dc == nullptr)
+        return false;
+    SDL_Rect rectangle;
+    rectangle.x = (int)(x0);
+    rectangle.y = (int)(y0);
+    rectangle.w = txLinUnportableModule(x1 - x0);
+    rectangle.h = txLinUnportableModule(y1 - y0);
+    SDL_RenderDrawRect(dc, &rectangle);
+    txRedrawWindow();
+    return true;
+}
+
+inline bool txPolygon (const POINT* points, int numPoints, HDC dc = txDC()) {
+    if (dc == nullptr)
+        return false;
+    SDL_Point* sdlPoints = txLinUnportablePointCapsToSDL(points, numPoints);
+    SDL_RenderDrawPoints(dc, sdlPoints, numPoints);
+    free(sdlPoints);
+    sdlPoints = nullptr;
+    txRedrawWindow();
+    return true;
+}
+
+inline bool txEllipse (double x0, double y0, double x1, double y1, HDC dc = txDC()) {
+    // does not work yet
+    return false;
+}
+
+inline bool txCircle (double x, double y, double r) {
+    int xtmp = (int)(r);
+    int ytmp = 0;
+    txSetPixel((int)(x) + xtmp, (int)(y) + ytmp);
+    if ((int)(r) > 0) {
+        txSetPixel((int)(x) + xtmp, (ytmp * (-1)) + (int)(y));
+        txSetPixel((int)(x) + ytmp, xtmp + (int)(y));
+        txSetPixel((int)(x) + (ytmp * (-1)), xtmp + (int)(y));
+    }
+    int P = 1 - (int)(r);
+    while (xtmp > ytmp) {
+        ytmp = ytmp + 1;
+        if (P <= 0)
+            P = P + (2 * ytmp) + 1;
+        else {
+            xtmp = xtmp - 1;
+            P = P + (2 * ytmp) - (2 * xtmp) + 1;
+        }
+        if (xtmp < ytmp)
+            break;
+        txSetPixel((int)(x) + xtmp, ytmp + (int)(y));
+        txSetPixel((int)(x) + (xtmp * (-1)), ytmp + (int)(y));
+        txSetPixel((int)(x) + xtmp, (ytmp * (-1)) + (int)(y));
+        txSetPixel((int)(x) + (xtmp * (-1)), (ytmp * (-1)) + (int)(y));
+        if (xtmp != ytmp) {
+            txSetPixel((int)(x) + ytmp, xtmp + (int)(y));
+            txSetPixel((int)(x) + ((-1) * ytmp), xtmp + (int)(y));
+            txSetPixel((int)(x) + ytmp, ((-1) * xtmp) + (int)(y));
+            txSetPixel((int)(x) + ((-1) * ytmp), ((-1) * xtmp) + (int)(y));
+        }
+    }
+    return true;
+}
+
+/*
+
+
+bool txArc (double x0, double y0, double x1, double y1, double startAngle, double totalAngle, HDC dc = txDC());
+
+
+bool txPie (double x0, double y0, double x1, double y1, double startAngle, double totalAngle, HDC dc = txDC());
+
+
+bool txChord (double x0, double y0, double x1, double y1, double startAngle, double totalAngle, HDC dc = txDC());
+
+
+bool txFloodFill (double x, double y, COLORREF color = TX_TRANSPARENT, DWORD mode = FLOODFILLSURFACE, HDC dc = txDC());
+*/
+
+bool txTriangle (double x1, double y1, double x2, double y2, double x3, double y3) {
+    if (txLine(x1, y1, x2, y2) && txLine(x2, y2, x3, y3) && txLine(x3, y3, x1, y1))
+        return true;
+    return false;
+}
+
+
+inline int txBegin() {
+    return -1;
+}
+
+inline int txEnd() {
+    return -1;
+}
+
+inline void txLinUnportableSDLEventLoop() {
+    SDL_Delay(1000);
+    bool stop = false;
+    SDL_Event* eventHandler = (SDL_Event*)(malloc(sizeof(SDL_Event)));
+    while (stop == false) {
+        while (SDL_PollEvent(eventHandler) != 0) {
+            if (eventHandler->type == SDL_QUIT) {
+                stop = true;
+                break;
+            }
+        }
+    }
+    return;
+}
+
+//HBRUSH txSetFillColor (COLORREF color, HDC dc = txDC());
